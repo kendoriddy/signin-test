e750fb8217236d186339df82de8a0a387377669e
Kehinde onifade <onifkay@gmail.com>
2024-06-17 11:44:27 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import SignIn from "./SignIn";
+import OtpDisplay from "./OtpDisplay";
 
-function App() {
+const App = () => {
+  const [email, setEmail] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>OTP Authentication</h1>
+      {!email ? <SignIn onSignIn={setEmail} /> : <OtpDisplay email={email} />}
     </div>
   );
-}
+};
 
 export default App;
