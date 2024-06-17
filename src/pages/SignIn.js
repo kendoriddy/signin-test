1e89cbbc84deceaa1850054be9d4969d920fa12c
Kehinde onifade <onifkay@gmail.com>
2024-06-17 11:42:32 +0100

    ïnitial signin screen created

---
+++ src/pages/SignIn.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { auth, provider, signInWithPopup } from "../utils/firebase";
+
+const SignIn = ({ onSignIn }) => {
+  const handleSignIn = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      onSignIn(user.email);
+    } catch (error) {
+      console.error("Error during sign-in:", error);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleSignIn}>Sign In with Google</button>
+    </div>
+  );
+};
+
+export default SignIn;
