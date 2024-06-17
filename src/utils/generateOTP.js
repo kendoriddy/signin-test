b7758be45c66dabc09608437b382b2e1f329d2d9
Kehinde onifade <onifkay@gmail.com>
2024-06-17 13:14:03 +0100

    revert back to using js random number generation

--- src/utils/generateOTP.js
+++ src/utils/generateOTP.js
@@ -1,12 +1,3 @@
-import { totp } from "otplib";
-
-const secret = "KVKFKRCPNZQUYMLXOVYDSQKJKZDTSRLD";
-
 export const generateOTP = () => {
-  try {
-    return totp.generate(secret);
-  } catch (error) {
-    console.error("Error generating OTP:", error);
-    throw new Error("Failed to generate OTP");
-  }
+  return Math.floor(100000 + Math.random() * 900000).toString();
 };
